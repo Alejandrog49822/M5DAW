3c33964fd27dbec6d2c9ee59b890cad3f79a3cc1
Alejandrog49822 <119672539+Alejandrog49822@users.noreply.github.com>
2023-06-09 11:23:04 +0200

    Update LanchaAcuatica.h

--- Lanchas/LanchaAcuatica.h
+++ Lanchas/LanchaAcuatica.h
@@ -7,32 +7,32 @@ using namespace std;
 class LanchaAcuatica {
 
 private:
-    // Atributos
+    
     string nombre;
     int velocidad;
     int distanciaRecorrida;
     int nitroDisponible;
 
 public:
-    //Getters
+    
     string getNombre() ;
     int getVelocidad() ;
     int getDistanciaRecorrida();
     int getNitroDisponible();
 
-    //Seters
+    
     string setNombre(string pNombre);
     int setVelocidad(int pVelocidad) ;
     int setDistanciaRecorrida(int pDistanciaRecorrida) ;
     int setNitroDisponible(int pNitroDisponible);
 
-    // Métodos
+    
     void lanzarDado();
     bool puedeUsarNitro() ;
     void usarNitro();
     void calcularDistanciaRecorrida();    
 
-    // Constructor
+   
     LanchaAcuatica(string pNombre, int pVelocidad, int pDistanciaRecorrida, int pNitroDisponible);
 };
 
