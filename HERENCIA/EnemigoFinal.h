4323c0eeb241920a5c01806be96540ae1da0ebec
Alejandrog49822 <119672539+Alejandrog49822@users.noreply.github.com>
2023-06-20 09:34:28 +0200

    Add files via upload

---
+++ HERENCIA/EnemigoFinal.h
@@ -0,0 +1,12 @@
+#ifndef ENEMIGO_FINAL_H
+#define ENEMIGO_FINAL_H
+
+#include "Enemigo.h"
+// Clase EnemigoFinal que hereda de Enemigo
+class EnemigoFinal : public Enemigo {
+public:
+    EnemigoFinal();
+};
+
+#endif
+
